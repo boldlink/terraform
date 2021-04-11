a661bee6cdcfeaaa7ca911559d90cd57c3e95108
Hugo Almeida <hugo.almeida@boldlink.io>
2021-04-11 18:28:52 +0100

    UPG: sqs 0.13+

--- modules/aws/sqs/policy/vars.tf
+++ modules/aws/sqs/policy/vars.tf
@@ -1,8 +1,8 @@
 variable "queue_url" {
   description = "(Required) The URL of the SQS Queue to which to attach the policy"
-  default = ""
+  default     = ""
 }
 variable "policy" {
   description = "(Required) The JSON policy for the SQS queue."
-  default = ""
+  default     = ""
 }
