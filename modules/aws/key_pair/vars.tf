ba662728b0d145f6b365fdc79565de64b309784b
Hugo Almeida <hugo.almeida@boldlink.io>
2021-04-11 17:56:00 +0100

    UPG: ec2 key pair 0.13+

--- modules/aws/key_pair/vars.tf
+++ modules/aws/key_pair/vars.tf
@@ -1,10 +1,10 @@
-
 variable "key_name" {
   description = "Name to reference the key"
-  default = ""
+  default     = ""
 }
 
 variable "public_key" {
   description = "Create the AWS key pair to use on the initial setup WIP: to script key replacement from vault"
-  default = ""
+  default     = ""
 }
+
